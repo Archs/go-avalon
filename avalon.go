3b1abb84b54447b8db0cec9a3573bebdb980d6a9
Archs <sun@archs.tk>
2014-05-28 20:13:03 +0800

    add avalon.Element

--- avalon.go
+++ avalon.go
@@ -244,10 +244,10 @@ func Require(cb Callback, deps ...string) {
 
 // css( node, name, value?)，如果只有两个参数，读取元素的某个样式，
 // 三个参数时，设置元素某个样式
-func CSS(node js.Object, name string) js.Object {
-	return js.Global.Get(AV).Call("css", name)
+func Css(node js.Object, name string) string {
+	return js.Global.Get(AV).Call("css", name).Str()
 }
-func SetCSS(node js.Object, name string, value interface{}) {
+func SetCss(node js.Object, name string, value interface{}) {
 	js.Global.Get(AV).Call("css", name, value)
 }
 
@@ -275,4 +275,130 @@ func InnerHTML(node js.Object, str string) {
 // clearHTML(node)，清空元素的所有子节点。
 func ClearHTML(node js.Object) {
 	js.Global.Get(AV).Call("clearHTML", node)
+}
+
+type Element struct {
+	js.Object
+}
+
+func NewElement(id string) Element {
+	obj := js.Global.Get("document").Call("getElementById", id)
+	return Element{js.Global.Get(AV).Invoke(obj)}
+}
+
+// hasClass(cls)，判定有没有此类名
+func (e Element) HasClass(cls string) bool {
+	return e.Object.Call("hasClass", cls).Bool()
+}
+
+// addClass(cls)，只有元素不存在时才添加此类名（可同时添加多个）
+func (e Element) AddClass(cls string) {
+	e.Object.Call("addClass", cls)
+}
+
+// remvoeClass(cls)，移除多个类名
+func (e Element) AemvoeClass(cls string) {
+	e.Object.Call("remvoeClass", cls)
+}
+
+// toggleClass(cls, state?),切换多个类名，如果第2个参数为布尔，则根据它强行添加或删除类名
+func (e Element) ToggleClass(i ...interface{}) {
+	e.Object.Call("toggleClass", i...)
+}
+
+// attr(name,value?)， 读写特性（此方法非常弱，直接使用setAttribute, getAttribute实现，没有做任何兼容性处理）
+func (e Element) Attr(name string) {
+	e.Object.Call("attr", name)
+}
+
+func (e Element) SetAttr(name string, val interface{}) {
+	e.Object.Call("attr", name, val)
+}
+
+// data(name, value?)， 读写数据，使用HTML5的data-*特性实现。它会parse一下，让数据更为实用，思路同jQuery，如果一个传参也没有，将元素的data-*属性组成一个对象返回
+func (e Element) Data(name string) {
+	e.Object.Call("data", name)
+}
+
+func (e Element) SetData(name string, val interface{}) {
+	e.Object.Call("data", name, val)
+}
+
+// removeData(name)， 移除数据
+func (e Element) RemoveData(name string) {
+	e.Object.Call("removeData", name)
+}
+
+// css(name,value?)，读写样式，这个兼容性做得很好，因为长达一百行，连HTML5的私有前缀都能你补上。
+func (e Element) Css(name string) {
+	e.Object.Call("css", name)
+}
+
+func (e Element) SetCss(name string, val interface{}) {
+	e.Object.Call("css", name, val)
+}
+
+// width(val?), 读写宽度，注意对隐藏元素没有处理。
+func (e Element) Width() int {
+	return e.Object.Call("width").Int()
+}
+
+func (e Element) SetWidth(name string, val int) {
+	e.Object.Call("width", name, val)
+}
+
+// height(val?), 读写高度，注意对隐藏元素没有处理。
+func (e Element) Height() int {
+	return e.Object.Call("height").Int()
+}
+
+func (e Element) SetHeight(name string, val int) {
+	e.Object.Call("height", name, val)
+}
+
+// TODO refine bind/unbind
+// bind(type, fn, phase)，绑定事件，这个没有做链式操作，目的是为了返回回调给你卸载。
+func (e Element) Bind(typ, fn, phase interface{}) js.Object {
+	return e.Object.Call("bind", typ, fn, phase)
+}
+
+// unbind(type,fn, phase)，卸载事件。
+func (e Element) Unbind(typ, fn, phase interface{}) js.Object {
+	return e.Object.Call("unbind", typ, fn, phase)
+}
+
+// val，读取表单元素的value值，功能同jQuery。
+func (e Element) Val() string {
+	return e.Object.Call("val").Str()
+}
+
+func (e Element) SetVal(i interface{}) {
+	e.Object.Call("val", i)
+}
+
+type Coordinates struct {
+	Left int
+	Top  int
+}
+
+// offset，取得元素在文档中的坐标，功能只实现了jQuery的一半，只能读不能写。
+func (e Element) Offset() Coordinates {
+	obj := e.Object.Call("offset")
+	return Coordinates{Left: obj.Get("left").Int(), Top: obj.Get("top").Int()}
+}
+
+// scrollLeft，取得水平滚动条的位置。
+func (e Element) ScrollLeft() int {
+	return e.Object.Call("scrollLeft").Int()
+}
+func (e Element) SetScrollLeft(value int) {
+	e.Object.Call("scrollLeft", value)
+}
+
+// scrollTop，取得垂直滚动条的位置。
+func (e Element) ScrollTop() int {
+	return e.Object.Call("scrollTop").Int()
+}
+func (e Element) SetScrollTop(value int) {
+	e.Object.Call("scrollTop", value)
 }
